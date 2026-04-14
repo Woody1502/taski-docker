9a33dce23d48dc783b5a83a816f44a8914f405de
Alexey <woody.smolko@gmail.com>
2026-04-14 21:47:35 +0300

    csrf for admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,9 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '93.77.183.75 ', 'infrahomework.servepics.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '93.77.183.75', 'infrahomework.servepics.com']
+
+CSRF_TRUSTED_ORIGINS = ['https://infrahomework.servepics.com']
 
 
 # Application definition
